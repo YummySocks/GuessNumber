350091e9742b559cd7f2012869ca0dd88d1b18a7
Socks <patricksutcliffe123@gmail.com>
2022-04-09 12:17:18 -0400

    added wireframe for setting up input and questions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner answerObj = new Scanner(System.in);
+        String name = null;
+        String guess = null;
+        System.out.println("Hello! What is your name?");
+        name = answerObj.nextLine();
+        System.out.println("Hello"+ name + ", I am thinking of a number between 1 and 20");
+        System.out.println("Take a guess");
+        guess = answerObj.nextLine();
+        System.out.println("Hello you guessed " + guess);
+        System.out.println("Hello! What is your name?");
+
     }
 }
